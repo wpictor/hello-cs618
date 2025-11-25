0f76c19b8348202fd3d9d8d84f3c12e529cf7b18
wpictor <wpictor@gmail.com>
2025-11-25 00:57:47 +0000

    chore: mod 9 updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { eventRoutes } from './routes/events.js'
 
 const app = express()
 app.use(bodyParser.json())
@@ -10,6 +11,7 @@ app.use(cors())
 
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -1,14 +1,32 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
 import { getUserInfo } from '../api/users.js'
-
+import { useEffect, useState } from 'react'
+import { postTrackEvent } from '../api/events.js'
 import { Helmet } from 'react-helmet-async'
+import { PostStats } from '../components/PostStats.jsx'
 
 export function ViewPost({ postId }) {
+  const [session, setSession] = useState()
+  const trackEventMutation = useMutation({
+    mutationFn: (action) => postTrackEvent({ postId, action, session }),
+    onSuccess: (data) => setSession(data?.session),
+  })
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      trackEventMutation.mutate('startView')
+      timeout = null
+    }, 1000)
+    return () => {
+      if (timeout) clearTimeout(timeout)
+      else trackEventMutation.mutate('endView')
+    }
+  }, [])
+
   const postQuery = useQuery({
     queryKey: ['post', postId],
     queryFn: () => getPostById(postId),
@@ -53,7 +71,14 @@ export function ViewPost({ postId }) {
       <Link to='/'>Back to main page</Link>
       <br />
       <hr />
-      {post ? <Post {...post} fullPost /> : `Post with id${postId} not found.`}
+      {post ? (
+        <div>
+          <Post {...post} fullPost />
+          <hr /> <PostStats postId={postId} />
+        </div>
+      ) : (
+        `Post with id${postId} not found.`
+      )}
     </div>
   )
 }
